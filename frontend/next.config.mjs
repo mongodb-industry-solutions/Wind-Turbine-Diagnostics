6bd66c47b8ab92d66d9910bf89442f479fa15d67
meharsg96 <mehar.grewal@mongodb.com>
2024-10-24 17:39:19 +0530

    unified.env

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,15 +4,28 @@ import { join } from 'path';
 // Load environment variables from the root .env file
 dotenv.config({ path: join(process.cwd(), '../.env') });
 
+<<<<<<< Updated upstream
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+=======
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI, // Add this line to make the environment variable accessible at build time
+  },
+>>>>>>> Stashed changes
   rewrites: async () => {
     return [
       {
         source: '/embed-audio/:path*',
+<<<<<<< Updated upstream
         destination: process.env.NEXT_PUBLIC_ENV === 'local-compose'
           ? 'http://api:8000/embed-audio/:path*'
+=======
+        destination: process.env.NEXT_PUBLIC_ENV == 'local' ?
+          'http://api:8000/embed-audio/:path*'
+>>>>>>> Stashed changes
           : 'http://localhost:8000/embed-audio/:path*',
       },
     ];
